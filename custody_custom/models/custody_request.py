8300f94f032874b3c751fddf03ae700da7e9ab1c
root <root@vmi702542.contaboserver.net>
2025-03-02 23:54:53 +0100

    updating custody custom

--- custody_custom/models/custody_request.py
+++ custody_custom/models/custody_request.py
@@ -15,16 +15,28 @@ class CustodyRequest(models.Model):
         compute='_compute_second_approval',
         store=False
     )
+    is_third_approval = fields.Boolean(
+        compute='_compute_third_approval',
+        store=False
+    )
+    is_fourth_approval = fields.Boolean(
+        compute='_compute_fourth_approval',
+        store=False
+    )
+
     first_approval = fields.Many2one(related='employee_id.first_approval')
     second_approval = fields.Many2one(related='employee_id.second_approval')
+    third_approval = fields.Many2one(related='employee_id.third_approval')
+    fourth_approval = fields.Many2one(related='employee_id.fourth_approval')
     company_id = fields.Many2one(
         comodel_name="res.company", string="Company", index=1, default=lambda self: self.env.company
     )
 
-
-    state = fields.Selection(string="", selection=[('draft', 'Draft'), ('first_approve', 'First Approve'), ('approved', 'Approved'), ('paid', 'Paid'),
+    state = fields.Selection(string="", selection=[('draft', 'Draft'), ('first_approve', 'First Approve'),
+                                                   ('second_approve', 'Second Approve'),
+                                                   ('third_approve', 'Third Approve'), ('approved', 'Approved'),
+                                                   ('paid', 'Paid'),
                                                    ('settled', 'Settled'), ], required=False, default="draft")
-
 
     @api.model
     def create(self, vals):
@@ -37,18 +49,36 @@ class CustodyRequest(models.Model):
         """Compute if the current user is the first approver."""
         for rec in self:
             rec.is_first_approval = (
-                rec.employee_id
-                and rec.employee_id.first_approval
-                and rec.employee_id.first_approval.id == self.env.user.employee_id.id
+                    rec.employee_id
+                    and rec.employee_id.first_approval
+                    and rec.employee_id.first_approval.id == self.env.user.employee_id.id
             )
 
     def _compute_second_approval(self):
         """Compute if the current user is the second approver."""
         for rec in self:
             rec.is_second_approval = (
-                rec.employee_id
-                and rec.employee_id.second_approval
-                and rec.employee_id.second_approval.id == self.env.user.employee_id.id
+                    rec.employee_id
+                    and rec.employee_id.second_approval
+                    and rec.employee_id.second_approval.id == self.env.user.employee_id.id
+            )
+
+    def _compute_third_approval(self):
+        """Compute if the current user is the second approver."""
+        for rec in self:
+            rec.is_third_approval = (
+                    rec.employee_id
+                    and rec.employee_id.third_approval
+                    and rec.employee_id.third_approval.id == self.env.user.employee_id.id
+            )
+
+    def _compute_fourth_approval(self):
+        """Compute if the current user is the second approver."""
+        for rec in self:
+            rec.is_fourth_approval = (
+                    rec.employee_id
+                    and rec.employee_id.fourth_approval
+                    and rec.employee_id.fourth_approval.id == self.env.user.employee_id.id
             )
 
     def action_first_approve(self):
@@ -61,6 +91,23 @@ class CustodyRequest(models.Model):
                 mail_template = self.env.ref('custody_custom.send_second_approve_email')
                 mail_template.send_mail(rec.id, force_send=True, email_values={'res_id': rec.id})
 
+    def action_second_approve(self):
+        self = self.sudo()
+        for rec in self:
+            rec.write({'state': 'second_approve'})
+            if rec.requested_value <= rec.employee_id.second_amount_to:
+                rec.set_approved()
+            else:
+                mail_template = self.env.ref('custody_custom.send_second_approve_email')
+                mail_template.send_mail(rec.id, force_send=True, email_values={'res_id': rec.id})
 
-
+    def action_third_approve(self):
+        self = self.sudo()
+        for rec in self:
+            rec.write({'state': 'third_approve'})
+            if rec.requested_value <= rec.employee_id.third_amount_to:
+                rec.set_approved()
+            else:
+                mail_template = self.env.ref('custody_custom.send_second_approve_email')
+                mail_template.send_mail(rec.id, force_send=True, email_values={'res_id': rec.id})
 
